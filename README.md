7005444e3d74184f0c302c841aa08843025b4471
Pepe Valdivia <jvaldivia@softweb.pe>
2018-05-22 19:30:01 -0500

    Usando axios para las peticioens ajax

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 + https://router.vuejs.org/es/essentials/getting-started.html
 + https://github.com/fernandoggaitan/vuejs_ajax
++ https://vuejs.org/v2/guide/ssr.html
++ https://vuejs.org/v2/guide/
++ https://github.com/axios/axios
--- server.js
+++ server.js
@@ -34,3 +34,134 @@ console.log('Listening on port 9090');
 app.get('/', function (req, res) {
   res.redirect('/demos');
 });
+app.post('/archivo/subir',  bodyParser.text({ type: 'json' }), function (req, res) {
+  var key1 = req.body.key1;
+  var key2 = req.body.key2;
+  let sampleFile = req.files.myFile;
+  var tempFileNameArray = sampleFile.name.split(".");
+  var randomVal = makeId();
+  sampleFile.mv('uploads/' + randomVal + '.' + tempFileNameArray[tempFileNameArray.length - 1], function(err) {
+    if (err){
+      res.statusCode = 500;
+      res.send(err);
+    }
+    var rpta = {
+      'tipo_mensaje': 'success',
+      'mensaje': [
+        'Se ha registrado una nueva imagen', // mensaje
+        randomVal, // imagenId
+        base_url + 'uploads/' + randomVal + '.' + tempFileNameArray[tempFileNameArray.length - 1], // url imagen
+      ]
+    };
+    res.send(JSON.stringify(rpta));
+  });
+});
+app.get('/departamento/listar', function (req, res) {
+  unirest.get(servicio_url + 'departamento/listar')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.get('/distrito/buscar', function (req, res) {
+  unirest.get(servicio_url + 'distrito/buscar?nombre=' + req.query.nombre)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.post('/departamento/guardar', function (req, res) {
+  unirest.post(servicio_url + 'departamento/guardar?data=' + req.body.data)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.get('/provincia/listar/:departamentoId', function (req, res) {
+  unirest.get(servicio_url + 'provincia/listar/' + req.params.departamentoId)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.post('/provincia/guardar', function (req, res) {
+  unirest.post(servicio_url + 'provincia/guardar?data=' + req.body.data)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.get('/distrito/listar/:provinciaId', function (req, res) {
+  unirest.get(servicio_url + 'distrito/listar/' + req.params.provinciaId)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.get('/distrito/count', function (req, res) {
+  unirest.get(servicio_url + 'distrito/count')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.get('/distrito/buscar_pagina', function (req, res) {
+  //BASE_URL + distrito/buscar_pagina?data={"step":10,"page":10}
+  unirest.get(servicio_url + 'distrito/buscar_pagina?data=' + req.query.data)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.post('/distrito/guardar', function (req, res) {
+  unirest.post(servicio_url + 'distrito/guardar?data=' + req.body.data)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.get('/tipo_estacion/listar', function (req, res) {
+  unirest.get(servicio_url + 'tipo_estacion/listar')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.get('/estacion/listar', function (req, res) {
+  unirest.get(servicio_url + 'estacion/listar')
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
+app.post('/estacion/guardar', function (req, res) {
+  unirest.post(servicio_url + 'estacion/guardar?data=' + req.body.data)
+  .headers({'Accept': 'application/json', 'Content-Type': 'application/json'})
+  .send()
+  .end(function (response) {
+    //console.log(response.body);
+    res.send(response.body);
+  });
+});
